f0c3caad819a5468032ab0988226b1b5196250a3
shamik Mitra <mitrashamik@gmail.com>
2017-11-25 18:48:29 +0530

    Adding client

---
+++ POCClient/src/main/java/com/spring5/poc/POCClient/PocClientApplication.java
@@ -0,0 +1,12 @@
+package com.spring5.poc.POCClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocClientApplication.class, args);
+	}
+}
---
+++ POCClient/src/test/java/com/spring5/poc/POCClient/PocClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring5.poc.POCClient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
